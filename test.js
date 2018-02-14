134d7c62aaab4aa82ac555974a24fba609b43d3b
Patrick McGuckin <patrick@gskinner.com>
2018-02-14 13:47:53 -0700

    Add test harness.

---
+++ test.js
@@ -0,0 +1,17 @@
+import Test from './index';
+
+let t;
+
+describe('Test', () => {
+  test('instantiates', () => {
+    expect(() => t = new Test(5, 10)).not.toThrow();
+  });
+  test('multiplies', () => {
+    t = new Test(5, 10);
+    expect(t.multiply()).toBe(50);
+  });
+  test('divides', () => {
+    t = new Test(5, 2);
+    expect(t.divide()).toBe(2.5);
+  });
+});
